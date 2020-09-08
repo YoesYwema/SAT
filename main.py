0e6e4ec20d8cd044cadfd9b88cebbc706bd1cc78
Yoes Ywema <yoes@live.nl>
2020-09-08 15:42:29 +0200

    new code

--- main.py
+++ main.py
@@ -12,11 +12,17 @@ args = parser.parse_args()
 print("Opening file: ", args.inputfile)
 file = open(args.inputfile, "r+")     # opens the sudoku file
 
+def sudoku_into_dimac(file):
+    fileDimacs = open("fileD", "w+")  # write DIMAC version into a new file
+    """"Needs code to translate sudoku into DIMACS format  """
+    return fileDimacs
+
+
+fileDimac = sudoku_into_dimac(file)
 if args.strategy == 1:    # start solving sudokus with strategy 1
     print("You've chosen strategy", args.strategy)
 if args.strategy == 2:    # start solving sudokus with strategy 2
     print("You've chosen strategy", args.strategy)
 if args.strategy == 3:    # start solving sudokus with strategy 3
     print("You've chosen strategy", args.strategy)
-
 file.close()
