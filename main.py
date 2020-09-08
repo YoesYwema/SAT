32ee9d7cd5b7461c5e1d07bb900b04e00668a366
Yoes Ywema <yoes@live.nl>
2020-09-08 18:42:25 +0200

    Orgaised a little bit and added comments

--- main.py
+++ main.py
@@ -1,18 +1,5 @@
 import argparse
 import math
-parser = argparse.ArgumentParser(description="Tell me which strategy to use on which sudoku.")
-
-parser.add_argument("SAT", type=str, nargs='+')
-
-parser.add_argument("--strategy", "-S", type=int, default=1, choices=[1, 2, 3],
-                    help="Choose the strategy")
-
-parser.add_argument('inputfile', help="Choose your sudoku file to be solved")
-args = parser.parse_args()
-
-print("Opening file: ", args.inputfile)
-file = open(args.inputfile, "r+")     # opens the sudoku file
-
 
 def sudoku_into_dimac(file):
     file_dimacs = open("fileDimacs", "w+")  # opens new file to write DIMAC version into it
@@ -33,16 +20,28 @@ def sudoku_into_dimac(file):
             row += 1
         if row >= size:                 # if you are further then the last row -> go to the next sudoku
             row = 0
-            file_dimacs.write("\n")
+            file_dimacs.write("\n")     # print a new line when going to the next sudoku
             sudoku_nr += 1
     return file_dimacs
 
-fileDimac = sudoku_into_dimac(file)
 
+parser = argparse.ArgumentParser(description="Tell me which strategy to use on which sudokus.")
+
+parser.add_argument("SAT", type=str, nargs='+')
+
+parser.add_argument("--strategy", "-S", type=int, default=1, choices=[1, 2, 3],
+                    help="Choose the strategy")
+
+parser.add_argument('inputfile', help="Choose your sudoku file to be solved")
+args = parser.parse_args()
+
+print("Opening file: ", args.inputfile)
+file = open(args.inputfile, "r+")  # opens the sudoku file
+fileDimac = sudoku_into_dimac(file)
+file.close()
 if args.strategy == 1:    # start solving sudokus with strategy 1
     print("You've chosen strategy", args.strategy)
 if args.strategy == 2:    # start solving sudokus with strategy 2
     print("You've chosen strategy", args.strategy)
 if args.strategy == 3:    # start solving sudokus with strategy 3
     print("You've chosen strategy", args.strategy)
-file.close()
