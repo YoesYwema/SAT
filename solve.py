c8ff7171291221f8f91c69ea080e232e2a70d79b
Yoes Ywema <yoes@live.nl>
2020-09-14 15:24:54 +0200

    parser toegevoegd

--- solve.py
+++ solve.py
@@ -1,7 +1,21 @@
 def solve_sudoku(strategy):
-    sudoku = open("fileDimacs", "r").read().splitlines()
-    rules = open("sudoku-rules.txt", "r+").read().splitlines()
+    sudoku = parser("fileDimacs")
+    rules = parser("sudoku-rules.txt")
+    rules_copy = rules.copy()
+    pure_literals(rules, sudoku)
+
     """Here we need code to reduce the amount of clauses by the DPLL algorithm """
+
+def parser(file):
+    clauses = []
+    for line in open(file):
+        if line.startswith("p"):
+            nvars, nclauses = line.split()[2:4]
+            continue
+        clause = [int(y) for y in line[:-2].split()]
+        clauses.append(clause)
+    return clauses
+
 
 # def solve_sudoku_strategy1(strategy):
 #     if strategy == 1:  # start solving sudokus with strategy 2
@@ -21,9 +35,13 @@ def solve_sudoku(strategy):
 # def empty_clauses():
 #
 # def unit_clauses():
-#
-# def pure_literals():
-#
+
+def pure_literals(rules, sudoku):
+    for literal in sudoku:
+        if literal in rules:
+            rules.pop(literal)
+            print(rules)
+
 # def amount_of_clauses():
 #
 # def split():
